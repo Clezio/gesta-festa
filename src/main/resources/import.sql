4cbe93841ed3f54f2b8698ae1ee651d3a500b00a
Clezio Penha <clezio.penha@armateus.com.br>
2017-12-12 14:50:10 -0300

    Manutencao

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into convidado (nome, quantidade_acompanhantes) values ('Pedro', 2);
-insert into convidado (nome, quantidade_acompanhantes) values ('Maria', 3);
-insert into convidado (nome, quantidade_acompanhantes) values ('Ricardo', 1);
+insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Pedro', 2);
+insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Maria', 3);
+insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Ricardo', 1);
